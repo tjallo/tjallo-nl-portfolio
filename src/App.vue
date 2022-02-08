32df9742176b371239e4a6b21f8d89c07a4215d6
Tjalle <31376864+tjallo@users.noreply.github.com>
2022-02-08 22:11:34 +0100

    Basic router setup with tailwindcss navbar

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- <div class="wrapper"> -->
+  <!-- <h1 class="text-2xl font-bold underline">Hello world!</h1> -->
+
+  <Navbar />
+
+  <!-- <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </div>
+  -->
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+</script>
+
+<style>
+</style>
