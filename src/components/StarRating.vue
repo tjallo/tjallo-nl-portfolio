cafe30a97659997708c805971477a6217fb3b5b8
Tjalle <31376864+tjallo@users.noreply.github.com>
2022-02-09 00:16:56 +0100

    added custom starrating element

---
+++ src/components/StarRating.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <ul class="flex items-center">
+            <li v-for="index in (Number.parseInt(given))" :key="index">
+                <StarIcon class="yellow-star"></StarIcon>
+            </li>
+            <li v-for="index in (Number.parseInt(total) - Number.parseInt(given))" :key="index">
+                <StarIcon class="grey-star"></StarIcon>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { StarIcon } from "@heroicons/vue/solid"
+
+
+const props = defineProps({
+    given: String,
+    total: String
+})
+
+
+</script>
+
+<style>
+.yellow-star {
+    @apply w-6 h-6 text-yellow-300 -m-0.5;
+}
+.grey-star {
+    @apply w-6 h-6 text-gray-700 -m-0.5;
+}
+</style>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,34 +1,25 @@
 <template>
-  <div className="container px-5 py-10 mx-auto">
-    <div className="text-center mb-20">
-      <ChipIcon className="w-10 inline-block mb-4" />
+  <div class="container px-5 py-10 mx-auto">
+    <div class="text-center mb-20">
+      <ChipIcon class="w-10 inline-block mb-4" />
       <h1
-        className="sm:text-4xl text-3xl font-medium title-font text-white mb-4"
-      >
-        Skills &amp; Technologies
-      </h1>
-      <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+        class="sm:text-4xl text-3xl font-medium title-font text-white mb-4"
+      >Skills &amp; Technologies</h1>
+      <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit ipsa
         delectus eum quo voluptas aspernatur accusantium distinctio possimus
         est.
       </p>
     </div>
-    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-      <div
-        v-for="skill in skills"
-        :key="skill[0]"
-        className="p-2 sm:w-1/2 w-full"
-      >
-        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+    <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+      <div v-for="skill in skills" :key="skill.name" class="p-2 sm:w-1/2 w-full">
+        <div class="bg-gray-800 rounded flex p-4 h-full items-center">
           <!-- <BadgeCheckIcon
             className="text-green-400 w-6 h-6 flex-shrink-0 mr-4"
-          /> -->
+          />-->
 
-          <BadgeCheckIcon class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" />
-
-          <span className="title-font font-medium text-white">
-            {{ skill }}
-          </span>
+          <StarRating :given="skill.rating" total="5"></StarRating>
+          <span class="skill-name">{{ skill.name }}</span>
         </div>
       </div>
     </div>
@@ -36,10 +27,19 @@
 </template>
 
 <script setup lang="ts">
-const skills = ["Python", "Vue.js", "Node", "Scikit-learn", "Linux", "Bash"];
+const skills = [
+  { name: "Python", rating: "5" },
+  { name: "Node.js", rating: "5" },
+  { name: "Java", rating: "4" },
+  { name: "Vue.js", rating: "4" },
+  { name: "Docker", rating: "3" },
+];
 
-import { BadgeCheckIcon } from "@heroicons/vue/solid";
+import StarRating from '@/components/StarRating.vue';
 </script>
 
 <style>
+.skill-name {
+  @apply font-medium text-white pl-2;
+}
 </style>
